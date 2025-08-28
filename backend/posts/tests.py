e8dc49a89dadb015298ed9f80194ff81507f1a1c
solmaz-sahmani <solmaz.sahmani@gmail.com>
2025-08-28 23:14:45 +0330

    post tests

--- backend/posts/tests.py
+++ backend/posts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
 
-# Create your tests here.
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username = 'solmaz',
+            email = 'solmaz.sahmani@gmail.com',
+            password = 'secret'
+        )
+        
+        cls.post = Post.objects.create(
+            author = cls.user,
+            title = 'this is a title',
+            bocy = 'body content'
+        )    
+        
+    def test_post_model(self):
+        self.assertEqual(self.post.author.username, 'solmaz')
+        self.assertEqual(self.post.title,'this is a title')
+        self.assertEqual(self.post.body, 'body content')
+        self.assertEqual(str(self.post), 'this is a title')
+        
