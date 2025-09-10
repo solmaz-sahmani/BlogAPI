1e37d243fcc5de9f34ab3a2c0dbcefad8f02232c
solmaz-sahmani <solmaz.sahmani@gmail.com>
2025-09-10 22:23:30 +0330

    add userlist and userdetail

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from .permissions import IsAuthorOrReadOnly
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
@@ -13,3 +14,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer    
