61ac5c37eeaccc147994090b1feeb37518797394
solmaz-sahmani <solmaz.sahmani@gmail.com>
2025-09-11 12:24:25 +0330

    add swagger

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,6 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog-api",
+      default_version='v1',
+      description="This is a blog-api project with Swagger",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="solmaz.sahmani@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +38,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('swagger/api.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ]
