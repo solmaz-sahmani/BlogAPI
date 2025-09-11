0a479c330d7498605215dc843e8103566fb57b50
solmaz-sahmani <solmaz.sahmani@gmail.com>
2025-09-11 10:44:43 +0330

    rewrite posts urls with routers

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, PostViewSet
 
-from .views import PostList, PostDetail, UserList, UserDetail
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
-    path('', PostList.as_view(), name='post_list'),
-    path('users/', UserList.as_view(), name='user_list'),
-    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
-]
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
