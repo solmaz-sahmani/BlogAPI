7bda696bb1679bf2c14be7d7f46ccfc2ea30003d
solmaz-sahmani <solmaz.sahmani@gmail.com>
2025-09-15 10:44:09 +0330

    add register view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from .tasks import send_welcome_email
 
-# Create your views here.
+User = get_user_model()
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = make_password(request.POST["password"])
+        user = User.objects.create(username=username, email=email, password=password)
+
+        send_welcome_email.delay(user.email)
+
+        return render(request, "register_success.html")
+
+    return render(request, "register.html")
